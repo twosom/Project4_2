e1df17228af8a5013f596896139ab0266f5f770d
desirel <two_somang@icloud.com>
2021-01-13 14:29:19 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/project4_2/MainActivity.java
@@ -0,0 +1,92 @@
+package com.cookandroid.project4_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        TextView text1, text2;
+        CheckBox chkAgree;
+        RadioGroup rGroup1;
+        RadioButton rdoDog, rdoCat, rdoRabbit;
+        Button btnOK;
+        ImageView imgPet;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("애완동물 사진 보기");
+        text1 = (TextView) findViewById(R.id.Text1);
+        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
+
+        text2 = (TextView) findViewById(R.id.Text2);
+        rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
+        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
+        rdoCat = (RadioButton) findViewById(R.id.RdoCat);
+        rdoRabbit = (RadioButton) findViewById(R.id.RdoRabbit);
+
+        btnOK = (Button) findViewById(R.id.BtnOK);
+        imgPet = (ImageView) findViewById(R.id.ImgPet);
+
+        //변수 정의 끝
+
+        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (chkAgree.isChecked()) {
+                    text2.setVisibility(View.VISIBLE);
+                    rGroup1.setVisibility(View.VISIBLE);
+                    btnOK.setVisibility(View.VISIBLE);
+                    imgPet.setVisibility(View.VISIBLE);
+                } else {
+                    text2.setVisibility(View.INVISIBLE);
+                    rGroup1.setVisibility(View.INVISIBLE);
+                    btnOK.setVisibility(View.INVISIBLE);
+                    imgPet.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+        btnOK.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                switch (rGroup1.getCheckedRadioButtonId()) {
+                    case R.id.RdoDog:
+                        imgPet.setImageResource(R.drawable.dog);
+                        break;
+                    case R.id.RdoCat:
+                        imgPet.setImageResource(R.drawable.cat);
+                        break;
+                    case R.id.RdoRabbit:
+                        imgPet.setImageResource(R.drawable.rabbit);
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(), "동물 먼저 선택해주세요", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
+    }
+}
